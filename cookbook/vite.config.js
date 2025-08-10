8dc36f78ec90f1f4f8783961b7bf396eea400286
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-10 04:24:10 +0200

    nginxx15

--- cookbook/vite.config.js
+++ cookbook/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/receipt-cookbook/',
   build: {
     sourcemap: true, // :white_check_mark: Add this
   },
