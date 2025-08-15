c84e209a23da38ab59b52dd0c4e4e392850a9457
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-15 19:46:03 +0200

    header section2

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import './header.scss'
 export default function Header(){
     return <>
         <div className="header-sec">
-            <img className="name-logo" src="/src/assets/name.png" alt="logo-name"/>
             <img className="star-logo" src="/src/assets/star.png" alt="logo-star"/>
+            <img className="name-logo" src="/src/assets/name.png" alt="logo-name"/>
         </div>
     </>
 }
