0bf29f920d2e24e400bcdabc321706b2232e62b7
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-16 23:59:29 +0200

    photos moved to public

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -3,8 +3,9 @@ import './header.scss'
 export default function Header(){
     return <>
         <div className="header-sec">
-            <img className="star-logo" src="/src/assets/star.png" alt="logo-star"/>
-            <img className="name-logo" src="/src/assets/name.png" alt="logo-name"/>
+            {/*<img className="star-logo" src="/public/logo (13).png" alt="logo-star"/>*/}
+            <div></div>
+            <img className="name-logo" src="/public/name2.png" alt="logo-name"/>
         </div>
     </>
 }
