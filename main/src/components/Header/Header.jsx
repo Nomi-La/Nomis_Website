54f30a2ac72812dfcb94d881222220368cc719f2
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-17 00:11:41 +0200

    img src changed

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <div className="header-sec">
             {/*<img className="star-logo" src="/public/logo (13).png" alt="logo-star"/>*/}
             <div></div>
-            <img className="name-logo" src="/public/name2.png" alt="logo-name"/>
+            <img className="name-logo" src="/name2.png" alt="logo-name"/>
         </div>
     </>
 }
