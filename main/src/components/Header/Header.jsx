a4710d0e3253874afd18375c870da1843907361c
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-18 00:19:26 +0200

    some progres is made... better go sleep now:)

--- main/src/components/Header/Header.jsx
+++ main/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     const navigate = useNavigate()
     return <>
         <div className="header-sec">
-            <img className="name-logo" src="/name2.png" alt="logo-name" onClick={()=>navigate("/")}/>
+            <img className="name-logo" src="/long%20name%20(10).png" alt="logo-name" onClick={()=>navigate("/")}/>
             {/*<img className="star-logo" src="/logo%20(13).png" alt="logo-star"/>*/}
         </div>
     </>
