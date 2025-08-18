34da0e16a8a2f05386ddb27d8e56163339af4716
Nomis_Lenovo <nomilang6@gmail.com>
2025-08-18 23:08:35 +0200

    colors scss file implemented

--- main/src/components/Sidebar/Sidebar.jsx
+++ main/src/components/Sidebar/Sidebar.jsx
@@ -8,23 +8,22 @@ export default function Sidebar(){
     return <>
         <div className="sidebar-container">
             {image && <div className="image-wrapper">
-                <img className="profile" src="/serious%20(2).png" alt="profile"/>
+                <img className="profile" src="/casual%20(2).png" alt="profile"/>
                 <button onClick={()=>setImage(false)}>{">"}</button>
             </div>}
             {!image && <div className="image-wrapper">
                 <button onClick={()=>setImage(true)}>{"<"}</button>
-                <img className="profile" src="/casual%20(2).png" alt="profile"/>
+                <img className="profile" src="/serious%20(2).png" alt="profile"/>
 
             </div>}
-            <p><span className="welcome">Welcome to my website:)</span>
+            <p><span className="welcome">Welcome:)</span>
                 <br/><br/>I am <b>Nomi Lang</b>, a <b>Full-Stack developer</b>
                 <br/>-And <span className="much-more">so much more</span>! ↓</p>
+
+            <NavLink className="subject" to="/projects">Projects</NavLink>
             <NavLink className="subject" to="/about-me">About Me</NavLink>
-            <NavLink className="subject" to="/group-projects">Group Projects</NavLink>
-            <NavLink className="subject" to="/study-projects">Study Projects</NavLink>
-            <NavLink className="subject" to="/info">Why I created this website</NavLink>
-            <NavLink className="subject" to="/study-projects">How to reach me</NavLink>
-            <p className="subject">®</p>
+            <NavLink className="subject" to="/get-lost">Get Lost ♥</NavLink>
+            <p className="subject" style={{"font-size": "x-large", "font-weight": "100"}}>©</p>
 
         </div>
     </>
