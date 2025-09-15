429f8c267ab31047aea78a17ff3f6034b3154cdf
Nomis_Lenovo <nomilang10@gmail.com>
2025-09-15 13:19:04 +0200

    action decoration removed

--- backend/showcase/views.py
+++ backend/showcase/views.py
@@ -18,38 +18,8 @@ class ProjectListCreateView(FilterMixin, ListCreateAPIView):
         'user': 'section__user__id',
     }
 
-    @action(detail=False, methods=['post'])
-    def reorder(self, request):
-        ids = request.data.get('ids', [])
-        with transaction.atomic():
-            updates = [Project(id=pid, position=i) for i, pid in enumerate(ids, start=1)]
-            Project.objects.bulk_update(updates, ['position'])
-        return Response(status=204)
-
-
 class ProjectRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [IsProjectLinkOwnerOrReadOnly]
 
-    @action(detail=False, methods=['post'])
-    def reorder(self, request):
-        ids = request.data.get('ids', [])
-        with transaction.atomic():
-            updates = [Project(id=pid, position=i) for i, pid in enumerate(ids, start=1)]
-            Project.objects.bulk_update(updates, ['position'])
-        return Response(status=204)
-
-# class LinkListCreateView(FilterMixin, ListCreateAPIView):
-#     queryset = Link.objects.all()
-#     serializer_class = LinkSerializer
-#     permission_classes = [IsProjectLinkOwnerOrReadOnly]
-#     filter_fields = {
-#         'project': 'project__id'
-#     }
-
-
-# class LinkRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
-#     queryset = Link.objects.all()
-#     serializer_class = LinkSerializer
-#     permission_classes = [IsProjectLinkOwnerOrReadOnly]
